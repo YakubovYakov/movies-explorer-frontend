0973e5df3e14f765249376de5b5e27465d6030df
Яков Якубов <a777@MacBook-Pro.local>
2023-10-19 04:59:35 +0300

    fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,38 +10,37 @@ function Header({ loggedIn, onMenuPopup }) {
     window.location.pathname === "/saved-movies" ||
     window.location.pathname === "/profile";
 
-		return (
-			<header className={isMovies ? "header header_type_movies" : "header"}>
-				<Link to="/">
-					<img className="header__logo" src={logo} alt="Логотип" />
-				</Link>
-				{loggedIn ? (
-					// <div className="header__content">
-						<div className="header__navigation">
-							<Navigation onMenuPopup={onMenuPopup} />
-						</div>
-					// </div>
-				) : (
-					// <div className="header__content">
-						<div className="header__links">
-							<Link
-								className="header__links-item header__links-item_register"
-								to="/signup"
-							>
-								Регистрация
-							</Link>
-							<Link
-								to="/signin"
-								className="header__links-item header__links-item_login"
-							>
-								Войти
-							</Link>
-						</div>
-					// </div>
-				)}
-			</header>
-		);
-		
+  return (
+    <header className={isMovies ? "header header_type_movies" : "header"}>
+      <Link to="/">
+        <img className="header__logo" src={logo} alt="Логотип" />
+      </Link>
+      {loggedIn ? (
+        <div className="header__content">
+          <div className="header__navigation">
+            <Navigation onMenuPopup={onMenuPopup} />
+          </div>
+        </div>
+      ) : (
+        <div className="header__content">
+          <div className="header__links">
+            <Link
+              className="header__links-item header__links-item_register"
+              to="/signup"
+            >
+              Регистрация
+            </Link>
+            <Link
+              to="/signin"
+              className="header__links-item header__links-item_login"
+            >
+              Войти
+            </Link>
+          </div>
+        </div>
+      )}
+    </header>
+  );
 }
 
 export default Header;
